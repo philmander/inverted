0eef0dac796029ab4977b202018c38c70d7897f9
Phil Mander <philip.mander@gmail.com>
2011-07-20 23:52:56 +0200

    Small fix to _createInstance
    Signed-off-by: Phil Mander <philip.mander@gmail.com>

--- src/main/ioc/ProtoFactory.js
+++ src/main/ioc/ProtoFactory.js
@@ -113,7 +113,7 @@
 		case 9:
 			instance = new proto(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
 			break;
-		case 9:
+		case 10:
 			instance = new proto(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8],
 				args[9]);
 			break;
