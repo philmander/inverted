53c43e8377f20164d3cbcb7f203c35d281929dcd
Phil Mander <philip.mander@gmail.com>
2011-10-02 20:47:12 +0200

    Tweaked structure: dev vs build
    Signed-off-by: Phil Mander <philip.mander@gmail.com>

--- src/main/inverted/release/expose.js
+++ src/main/inverted/release/expose.js
@@ -1,7 +1,6 @@
-//sub modules
-var inverted = {};
-var global = global || this;
-
 //expose as export for common js or global for browser
-var exports = exports || global;
-exports.inverted = inverted;
+if(typeof window === "undefined") {
+	exports.AppContext = inverted.AppContext;
+} else {
+	global.inverted = inverted;
+}
---
+++ src/main/inverted/release/init.js
@@ -0,0 +1,3 @@
+//include within build after intro or standalone before other js files in dev
+var global = global || this;
+var inverted = {};
