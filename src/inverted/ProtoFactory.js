2a2d2ba12e60bd83a4e47b4fc1f1255cfaaf60e5
Phil Mander <philip.mander@gmail.com>
2012-12-28 21:41:44 -0500

    Spaces for tabs
    philip.mander@gmail.com

--- src/inverted/ProtoFactory.js
+++ src/inverted/ProtoFactory.js
@@ -5,229 +5,229 @@ define("inverted/ProtoFactory", function() {
 
     "use strict";
 
-	/**
-	 * Create a new ProtoFactory with config
-	 * 
-	 * @constructor
-	 * @param {Object} config
-	 */
-	var ProtoFactory = function(config) {
+    /**
+     * Create a new ProtoFactory with config
+     * 
+     * @constructor
+     * @param {Object} config
+     */
+    var ProtoFactory = function(config) {
 
-		this.config = config;
+        this.config = config;
 
         //cache of loaded dependencies
-		this.dependencyMap = {};
-	};
+        this.dependencyMap = {};
+    };
 
 
-	/**
-	 * Adds dependency id to object/function mappings to the internal dependency map
+    /**
+     * Adds dependency id to object/function mappings to the internal dependency map
      * @param {Object} depMap   adds a map of dependencies to the existing dependency map cache
-	 */
-	ProtoFactory.prototype.addDependencies = function(depMap) {
-		
-		for(var depId in depMap) {
-			if(depMap.hasOwnProperty(depId)) {
-				this.dependencyMap[depId] = depMap[depId];				
-			}
-		}
-	};	
+     */
+    ProtoFactory.prototype.addDependencies = function(depMap) {
+        
+        for(var depId in depMap) {
+            if(depMap.hasOwnProperty(depId)) {
+                this.dependencyMap[depId] = depMap[depId];                
+            }
+        }
+    };    
 
-	/**
-	 * Gets a proto given an id
-	 * 
-	 * @param {String} id
-	 * @return {Object} A reference to a javascript object
-	 */
-	ProtoFactory.prototype.getProto = function(id) {
+    /**
+     * Gets a proto given an id
+     * 
+     * @param {String} id
+     * @return {Object} A reference to a javascript object
+     */
+    ProtoFactory.prototype.getProto = function(id) {
 
-		var protoData = this.getProtoConfig(id);
+        var protoData = this.getProtoConfig(id);
 
-		var instance = null;
+        var instance = null;
 
-		// for static just get a reference
-		if(protoData.scope === "static") {
+        // for static just get a reference
+        if(protoData.scope === "static") {
 
-			if(typeof protoData.module === "string") {
-				instance = this.dependencyMap[protoData.module];
-			}
-		// create an instance if not singleton or singleton and no instance
-		// defined yet (lazy loaded singletons)
-		} else if((!protoData.scope || protoData.scope !== "singleton") ||
-				(protoData.scope === "singleton" && !protoData.instance)) {
+            if(typeof protoData.module === "string") {
+                instance = this.dependencyMap[protoData.module];
+            }
+        // create an instance if not singleton or singleton and no instance
+        // defined yet (lazy loaded singletons)
+        } else if((!protoData.scope || protoData.scope !== "singleton") ||
+                (protoData.scope === "singleton" && !protoData.instance)) {
 
-			instance = this._createInstance(protoData.module, protoData.args, protoData.props, protoData.extendsRef);
+            instance = this._createInstance(protoData.module, protoData.args, protoData.props, protoData.extendsRef);
 
-			// save instance if singleton
-			if(protoData.scope && protoData.scope === "singleton") {
-				protoData.instance = instance;
-			}
-		}
-		// its a singleton and instance has already been created
-		else {
-			instance = protoData.instance;
-		}
+            // save instance if singleton
+            if(protoData.scope && protoData.scope === "singleton") {
+                protoData.instance = instance;
+            }
+        }
+        // its a singleton and instance has already been created
+        else {
+            instance = protoData.instance;
+        }
 
-		return instance;
-	};
+        return instance;
+    };
 
-	/**
-	 * Uses factory config to create a new instance
-	 *
+    /**
+     * Uses factory config to create a new instance
+     *
      * @param proto {String|Object} this might be a string or an proto
-	 * @param argData
-	 * @param propData
+     * @param argData
+     * @param propData
      * @param extendsRef
-	 * @return
-	 */
-	ProtoFactory.prototype._createInstance = function(proto, argData, propData, extendsRef) {
+     * @return
+     */
+    ProtoFactory.prototype._createInstance = function(proto, argData, propData, extendsRef) {
 
-		var instance = null;
+        var instance = null;
 
         //if the proto is a
-		if(typeof proto === "string") {
-			proto = this.dependencyMap[proto];
-		}
+        if(typeof proto === "string") {
+            proto = this.dependencyMap[proto];
+        }
 
-		// constructor injection
-		var args = this._createArgs(argData);
+        // constructor injection
+        var args = this._createArgs(argData);
 
-		// inheritance
-		if(extendsRef) {
-			this._extendProto(proto, this.getProto(extendsRef));
-		}
+        // inheritance
+        if(extendsRef) {
+            this._extendProto(proto, this.getProto(extendsRef));
+        }
 
-		// ugly but works. would like a better way
-		switch(args.length) {
-		case 0:
-			instance = new proto();
-			break;
-		case 1:
-			instance = new proto(args[0]);
-			break;
-		case 2:
-			instance = new proto(args[0], args[1]);
-			break;
-		case 3:
-			instance = new proto(args[0], args[1], args[2]);
-			break;
-		case 4:
-			instance = new proto(args[0], args[1], args[2], args[3]);
-			break;
-		case 5:
-			instance = new proto(args[0], args[1], args[2], args[3], args[4]);
-			break;
-		case 6:
-			instance = new proto(args[0], args[1], args[2], args[3], args[4], args[5]);
-			break;
-		case 7:
-			instance = new proto(args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
-			break;
-		case 8:
-			instance = new proto(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
-			break;
-		case 9:
-			instance = new proto(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
-			break;
-		case 10:
-			instance = new proto(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8],
-					args[9]);
-			break;
-		default:
-			throw new Error("Instances have 10 arg limit");
-		}
+        // ugly but works. would like a better way
+        switch(args.length) {
+        case 0:
+            instance = new proto();
+            break;
+        case 1:
+            instance = new proto(args[0]);
+            break;
+        case 2:
+            instance = new proto(args[0], args[1]);
+            break;
+        case 3:
+            instance = new proto(args[0], args[1], args[2]);
+            break;
+        case 4:
+            instance = new proto(args[0], args[1], args[2], args[3]);
+            break;
+        case 5:
+            instance = new proto(args[0], args[1], args[2], args[3], args[4]);
+            break;
+        case 6:
+            instance = new proto(args[0], args[1], args[2], args[3], args[4], args[5]);
+            break;
+        case 7:
+            instance = new proto(args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
+            break;
+        case 8:
+            instance = new proto(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
+            break;
+        case 9:
+            instance = new proto(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
+            break;
+        case 10:
+            instance = new proto(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8],
+                    args[9]);
+            break;
+        default:
+            throw new Error("Instances have 10 arg limit");
+        }
 
-		// property injection
-		if(propData) {
-			for( var propName in propData) {
-				if(propData.hasOwnProperty(propName)) {
-					var propertyArgs = this._createArgs([ propData[propName] ]);
+        // property injection
+        if(propData) {
+            for( var propName in propData) {
+                if(propData.hasOwnProperty(propName)) {
+                    var propertyArgs = this._createArgs([ propData[propName] ]);
 
-					if(typeof instance[propName] === "function") {
-						instance[propName].apply(instance, propertyArgs[0]);
-					} else {
-						instance[propName] = propertyArgs[0];
-					}
-				}
-			}
-		}
+                    if(typeof instance[propName] === "function") {
+                        instance[propName].apply(instance, propertyArgs[0]);
+                    } else {
+                        instance[propName] = propertyArgs[0];
+                    }
+                }
+            }
+        }
 
-		return instance;
-	};
+        return instance;
+    };
 
-	/**
-	 * Uses factory config to create a new instance
-	 * 
-	 * @param factoryRef
-	 * @param factoryMethod
-	 * @return
-	 */
-	ProtoFactory.prototype._getProtoFromFactory = function(factoryRef, factoryMethod) {
+    /**
+     * Uses factory config to create a new instance
+     * 
+     * @param factoryRef
+     * @param factoryMethod
+     * @return
+     */
+    ProtoFactory.prototype._getProtoFromFactory = function(factoryRef, factoryMethod) {
 
-		var factory = this.getProto(factoryRef);
+        var factory = this.getProto(factoryRef);
 
-		if(factoryMethod) {
-			return factory[factoryMethod].apply(factory);
-		} else {
-			throw new Error("No factory method defined with " + factoryRef);
-		}
-	};
+        if(factoryMethod) {
+            return factory[factoryMethod].apply(factory);
+        } else {
+            throw new Error("No factory method defined with " + factoryRef);
+        }
+    };
 
-	/**
-	 * Scans arg config for values, generating dependencies where required
-	 * 
-	 * @param {Array} confArgs
-	 * @return {Array}
-	 */
-	ProtoFactory.prototype._createArgs = function(confArgs) {
+    /**
+     * Scans arg config for values, generating dependencies where required
+     * 
+     * @param {Array} confArgs
+     * @return {Array}
+     */
+    ProtoFactory.prototype._createArgs = function(confArgs) {
 
-		// figure out constructors
-		var args = [];
-		if(confArgs) {
-			for( var i = 0; i < confArgs.length; i++) {
-				var argData = confArgs[i];
+        // figure out constructors
+        var args = [];
+        if(confArgs) {
+            for( var i = 0; i < confArgs.length; i++) {
+                var argData = confArgs[i];
 
-				// easier to deal with nulls here)
-				if(argData === null || typeof argData === "undefined") {
-					args[i] = argData;
-					continue;
-				}
+                // easier to deal with nulls here)
+                if(argData === null || typeof argData === "undefined") {
+                    args[i] = argData;
+                    continue;
+                }
 
-				var isObject = typeof argData === "object";
+                var isObject = typeof argData === "object";
 
-				// if arg has ref
-				if((isObject && argData.ref) || (typeof argData === "string" && argData.match(/^\*[^\*]/) !== null)) {
-					args[i] = this.getProto(argData.ref || argData.substr(1));
-				} else if(isObject && argData.factoryRef) {
-					args[i] = this._getProtoFromFactory(argData.factoryRef, argData.factoryMethod);
-				} else if(isObject && argData.proto) {
-					args[i] = this._createInstance(argData.module, argData.args, argData.props);
-				} else if(isObject) {
-					args[i] = {};
-					// if arg is object containing values
-					for( var key in argData) {
-						if(argData.hasOwnProperty(key)) {
-							var obj = argData[key];
+                // if arg has ref
+                if((isObject && argData.ref) || (typeof argData === "string" && argData.match(/^\*[^\*]/) !== null)) {
+                    args[i] = this.getProto(argData.ref || argData.substr(1));
+                } else if(isObject && argData.factoryRef) {
+                    args[i] = this._getProtoFromFactory(argData.factoryRef, argData.factoryMethod);
+                } else if(isObject && argData.proto) {
+                    args[i] = this._createInstance(argData.module, argData.args, argData.props);
+                } else if(isObject) {
+                    args[i] = {};
+                    // if arg is object containing values
+                    for( var key in argData) {
+                        if(argData.hasOwnProperty(key)) {
+                            var obj = argData[key];
 
-							if(obj && (obj.ref || (typeof obj === "string" && obj.match(/^\*[^\*]/) !== null))) {
-								args[i][key] = this.getProto(obj.ref || obj.substr(1));
-							} else if(obj && obj.factoryRef) {
-								args[i][key] = this._getProtoFromFactory(obj.factoryRef, obj.factoryMethod);
-							} else if(obj && obj.module) {
-								args[i][key] = this._createInstance(obj.module, obj.args, obj.props);
-							} else {
-								args[i][key] = obj;
-							}
-						}
-					}
-				} else {
-					// just a value
-					args[i] = argData;
-				}
-			}
-		}
-		return args;
-	};
+                            if(obj && (obj.ref || (typeof obj === "string" && obj.match(/^\*[^\*]/) !== null))) {
+                                args[i][key] = this.getProto(obj.ref || obj.substr(1));
+                            } else if(obj && obj.factoryRef) {
+                                args[i][key] = this._getProtoFromFactory(obj.factoryRef, obj.factoryMethod);
+                            } else if(obj && obj.module) {
+                                args[i][key] = this._createInstance(obj.module, obj.args, obj.props);
+                            } else {
+                                args[i][key] = obj;
+                            }
+                        }
+                    }
+                } else {
+                    // just a value
+                    args[i] = argData;
+                }
+            }
+        }
+        return args;
+    };
 
     /**
      * Extends one proto with another
@@ -235,76 +235,76 @@ define("inverted/ProtoFactory", function() {
      * @param {Object} superProto The base proto
      * @return {Object}
      */
-	ProtoFactory.prototype._extendProto = function(proto, superProto) {
+    ProtoFactory.prototype._extendProto = function(proto, superProto) {
 
-		// backup methods/props
-		var methods = {};
-		for( var method in proto.prototype) {
-			methods[method] = proto.prototype[method];
-		}
+        // backup methods/props
+        var methods = {};
+        for( var method in proto.prototype) {
+            methods[method] = proto.prototype[method];
+        }
 
-		// extend prototype
-		proto.prototype = superProto;
-		proto.prototype._super = superProto.constructor;
+        // extend prototype
+        proto.prototype = superProto;
+        proto.prototype._super = superProto.constructor;
 
-		// put methods back
-		for( var methodBackup in methods) {
-			proto.prototype[methodBackup] = methods[methodBackup];
-		}
+        // put methods back
+        for( var methodBackup in methods) {
+            proto.prototype[methodBackup] = methods[methodBackup];
+        }
 
-		// fix the contructor
-		proto.prototype.constructor = proto;
-	};
+        // fix the contructor
+        proto.prototype.constructor = proto;
+    };
 
-	/**
-	 * Searches the config for a proto matching the specified id
-	 * 
-	 * @param {String} id
-	 * @return {Object}
-	 */
-	ProtoFactory.prototype.getProtoConfig = function(id) {
+    /**
+     * Searches the config for a proto matching the specified id
+     * 
+     * @param {String} id
+     * @return {Object}
+     */
+    ProtoFactory.prototype.getProtoConfig = function(id) {
 
-		var config = this.config;
+        var config = this.config;
 
-		if(this.config.hasOwnProperty(id)) {
-			return config[id];
-		} else {
-			throw new Error("No proto is defined for " + id);
-		}
-	};
-	
-	var Util = {};
-	
-	/**
-	 * Strict check to see if an object is an array.
-	 */
-	Util.isArray = function(obj) {
+        if(this.config.hasOwnProperty(id)) {
+            return config[id];
+        } else {
+            throw new Error("No proto is defined for " + id);
+        }
+    };
+    
+    var Util = {};
+    
+    /**
+     * Strict check to see if an object is an array.
+     */
+    Util.isArray = function(obj) {
 
         if(Array.isArray) {
             return Array.isArray(obj);
         }
         return Object.prototype.toString.call(obj) === "[object Array]";
-	};
+    };
 
-	/**
-	 * Taken directly from jquery
-	 */
-	Util.inArray = function(elem, array) {
+    /**
+     * Taken directly from jquery
+     */
+    Util.inArray = function(elem, array) {
 
-		if(array.indexOf) {
-			return array.indexOf(elem);
-		}
+        if(array.indexOf) {
+            return array.indexOf(elem);
+        }
 
-		for( var i = 0, length = array.length; i < length; i++) {
-			if(array[i] === elem) {
-				return i;
-			}
-		}
+        for( var i = 0, length = array.length; i < length; i++) {
+            if(array[i] === elem) {
+                return i;
+            }
+        }
 
-		return -1;
-	};
+        return -1;
+    };
 
     //expose constructor
-	return ProtoFactory;
+    return ProtoFactory;
 
 });
