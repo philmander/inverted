6a36b1a7bd20471913794c9c03ce691e8fece024
Phil Mander <philip.mander@gmail.com>
2013-01-02 11:54:14 -0500

    Adding method injection test and renaming app-conf file
    Signed-off-by: Phil Mander <philip.mander@gmail.com>

--- test/resources/amd/D.js
+++ test/resources/amd/D.js
@@ -5,11 +5,16 @@ define(function() {
 		this.number = null;
 		this.bool = null;
 		this.nully = null;
+        this.fromMethod = null;
 	};
 	
 	D.prototype.bye = function() {
 		return "Bye!";
 	};
+
+    D.prototype.setter = function(value) {
+        this.fromMethod = value;
+    };
 	
 	return D;
 });
